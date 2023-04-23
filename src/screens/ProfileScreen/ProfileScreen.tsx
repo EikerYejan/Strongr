809cba4253423d31b7ff6ff0f9ffbb4d205e7b45
EikerYejan <eikeryejan@gmail.com>
2023-04-23 19:12:04 -0400

    update profile screen options

--- src/screens/ProfileScreen/ProfileScreen.tsx
+++ src/screens/ProfileScreen/ProfileScreen.tsx
@@ -1,28 +1,42 @@
-import {Alert, View} from "react-native"
+import {Alert} from "react-native"
 import RNRestart from "react-native-restart"
 
 // components
 import {MenuOption} from "@strongr/components/MenuOption/MenuOption"
+import {ScreenWrapper} from "@strongr/components/ScreenWrapper/ScreenWrapper"
 
 // constants
 import {COLORS} from "@strongr/constants/colors"
+import {NAVIGATORS, SCREEN_NAMES} from "@strongr/constants/screens"
 
 // storage
 import {Storage} from "@strongr/store/storage"
 
 // types
 import type {NavigationProp} from "@react-navigation/native"
-import {NAVIGATORS, SCREEN_NAMES} from "@strongr/constants/screens"
 
 interface Props {
   navigation: NavigationProp<Record<string, unknown>>
 }
 
 export const ProfileScreen = ({navigation}: Props) => {
-  const DEV = process.env.NODE_ENV === "development"
-
   const onOptionPress = (screen: string) => {
     navigation.navigate(NAVIGATORS.PROFILE_NAVIGATOR, {screen})
+  }
+
+  const onResetAppStoragePress = () => {
+    Alert.alert("Do you want to reset the app storage?", "", [
+      {
+        text: "Cancel"
+      },
+      {
+        text: "Reset",
+        onPress: () => {
+          Storage.clearAppStorage()
+          RNRestart.restart()
+        }
+      }
+    ])
   }
 
   const menuOptions = [
@@ -43,47 +57,32 @@ export const ProfileScreen = ({navigation}: Props) => {
       onPress: () => {
         onOptionPress(SCREEN_NAMES.SETTINGS)
       }
+    },
+    {
+      label: "Reset App Storage",
+      onPress: onResetAppStoragePress,
+      showIcon: false,
+      labelStyles: {
+        color: COLORS.PRIMARY,
+        fontSize: 17
+      }
+    },
+    {
+      disabled: true,
+      label: "Sign Out",
+      labelStyles: {
+        color: COLORS.ERROR,
+        fontSize: 17
+      },
+      showIcon: false
     }
   ]
 
-  const onResetAppStoragePress = () => {
-    Alert.alert("Do you want to reset the app storage?", "", [
-      {
-        text: "Cancel"
-      },
-      {
-        text: "Reset",
-        onPress: () => {
-          Storage.clearAppStorage()
-          RNRestart.restart()
-        }
-      }
-    ])
-  }
-
   return (
-    <View
-      style={{
-        paddingHorizontal: 32
-      }}
-    >
-      <View>
-        {menuOptions.map((option) => (
-          <MenuOption key={option.label} {...option} />
-        ))}
-        {DEV ? (
-          <MenuOption
-            label="Reset App Storage"
-            onPress={onResetAppStoragePress}
-          />
-        ) : null}
-        <MenuOption
-          disabled
-          label="Sign Out"
-          labelStyles={{color: COLORS.ERROR, fontSize: 17}}
-          showIcon={false}
-        />
-      </View>
-    </View>
+    <ScreenWrapper>
+      {menuOptions.map((option) => (
+        <MenuOption key={option.label} {...option} />
+      ))}
+    </ScreenWrapper>
   )
 }
