2d40d345a6cae3736c1bf800fccfe60bd48de684
Eiker Yejan <47006661+EikerYejan@users.noreply.github.com>
2023-04-30 22:08:39 -0400

    setup Xcode Cloud post_merge script (#1)
    * setup script take 2
    
    * take 3
    
    * take 4
    
    * take 5
    
    * fix failing deps

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+
+# Move to the root of the repository
+cd /Volumes/workspace/repository
+
 brew install cocoapods
 # have to add node yourself
 brew install node@18
@@ -11,6 +15,10 @@ brew install yarn
 
 # Install dependencies you manage with CocoaPods.
 yarn
+
+cd ./ios
+
 pod install
+
 # the sed command from RN cant find the file... so we have to run it ourselves
-sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
+# sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
