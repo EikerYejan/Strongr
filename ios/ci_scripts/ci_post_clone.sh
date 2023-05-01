bf3d2dfcf4b44deb13ff3b2bc5435ead56575d1d
EikerYejan <eikeryejan@gmail.com>
2023-04-30 21:31:27 -0400

    take 5

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+
+# Move to the root of the repository
+cd /Volumes/workspace/repository
+
 brew install cocoapods
 # have to add node yourself
 brew install node@18
@@ -8,12 +12,10 @@ brew install node@18
 brew link node@18
 
 brew install yarn
-
-# Move to the root of the repository
-cd /Volumes/workspace/repository
-
 # Install dependencies you manage with CocoaPods.
 yarn
-pod install
+
+
+cd ./ios && pod install
 # the sed command from RN cant find the file... so we have to run it ourselves
 # sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
