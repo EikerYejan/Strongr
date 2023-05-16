2a799b4f57b120cd8f39cadf633fec2b74a02cb7
EikerYejan <eikeryejan@gmail.com>
2023-05-16 14:18:57 -0400

    fix IOS build

--- ios/strongr/AppDelegate.mm
+++ ios/strongr/AppDelegate.mm
@@ -33,7 +33,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  RCTAppSetupPrepareApp(application, true);
+  RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [self.reactDelegate createBridgeWithDelegate:self launchOptions:launchOptions];
 
