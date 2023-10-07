b12c988d62651774fd3c5bdf6126fc8696b28bf2
EikerYejan <eikeryejan@gmail.com>
2023-10-07 19:33:41 -0400

    update TF release notes

--- ios/ci_scripts/ci_post_xcodebuild.sh
+++ ios/ci_scripts/ci_post_xcodebuild.sh
@@ -4,5 +4,5 @@
 if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
   TESTFLIGHT_DIR_PATH=../TestFlight
   mkdir $TESTFLIGHT_DIR_PATH
-  git fetch --deepen 3 && git log -3 --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+  git fetch --deepen 2 && git log -2 --pretty=format:"- %s -- %h %ci"  >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
 fi
